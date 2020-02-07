e9d34ff46e5646639dfac353776f7c879c678145
FabianHernandez <fabianhumbertohernandez@gmail.com>
2020-02-07 13:21:40 -0600

    validarCampo() validarLongitud()

--- js/app.js
+++ js/app.js
@@ -6,10 +6,32 @@ const btnEnviar = document.getElementById('enviar');
 eventListeners();
 
 function eventListeners(){
+  
   document.addEventListener('DOMContentLoaded', inicioApp);
+  
+  email.addEventListener('blur', validarCampo);
+  asunto.addEventListener('blur', validarCampo);
+  mensaje.addEventListener('blur', validarCampo);
 }
 
 function inicioApp(){
-  console.log('init');
+  
   btnEnviar.disabled = true;
+}
+
+function validarCampo(){
+  console.log('valCamp');
+  validarLongitud(this);
+}
+
+function validarLongitud(campo){
+  console.log(campo.value.length);
+  
+  if(campo.value.length > 3){
+    campo.style.borderBottomColor = 'Green';
+    campo.classList.remove('error');
+  } else {
+    campo.style.borderBottomColor = 'Red';
+    campo.classList.add('error');
+  }
 }
